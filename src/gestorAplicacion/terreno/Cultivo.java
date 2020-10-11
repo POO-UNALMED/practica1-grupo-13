7b74c0a216edf2523119effcdfa1ca5b3f7ce12a
José <jruedat@unal.edu.co>
2020-10-11 16:22:22 -0500

    Funciones de creacion de cultivos
    a

--- src/gestorAplicacion/terreno/Cultivo.java
+++ src/gestorAplicacion/terreno/Cultivo.java
@@ -16,10 +16,32 @@ public class Cultivo {
 	private int cantCampesino;
 	private boolean estadoAmenaza;
 	
-	public Cultivo(String tipoCultivo, int tamano, Amenaza amenaza, Terreno terreno, double cantProducida, double cantSembrada, double nitrogeno, double potasio,
-			double fosforo, double irrigacion, int cantCampesino, boolean estadoAmenaza) {
-		this.tipoCultivo = tipoCultivo; this.tamano = tamano; this.amenaza = amenaza; this.terreno = terreno; this.cantProducida = cantProducida;
-		this.cantSembrada = cantSembrada; this.nitrogeno = nitrogeno; this.potasio = potasio; this.fosforo = fosforo; this.irrigacion = irrigacion;
-		this.cantCampesino = cantCampesino; this.estadoAmenaza = estadoAmenaza;
+	public Cultivo(String tipoCultivo, int tamano, double nitrogeno, double potasio, double fosforo, double irrigacion) {
+		this.tipoCultivo = tipoCultivo; 
+		this.tamano = tamano; 
+		this.nitrogeno = nitrogeno; 
+		this.potasio = potasio; 
+		this.fosforo = fosforo; 
+		this.irrigacion = irrigacion;
 	}
+	
+	// Se le pide al usuario ingresar el tipo (dentro del Main), si es papa se ejecuta esta funcion o una de las siguientes:
+	static Cultivo crearPapa(int tamañoDeseado) {    
+		return (new Cultivo ("papa", tamañoDeseado, 0.3, 0.4, 0.4, 0.2));
+	}
+	static Cultivo crearSandia(int tamañoDeseado) {
+		return (new Cultivo ("sandia", tamañoDeseado, 0.1, 0.3, 0.5, 0.5));
+	}
+	static Cultivo crearMango(int tamañoDeseado) {
+		return (new Cultivo ("mango", tamañoDeseado, 0.5, 0.3, 0.2, 0.3));
+	}
+	static Cultivo crearBanano(int tamañoDeseado) {
+		return (new Cultivo ("banano", tamañoDeseado, 0.3, 0.1, 0.2, 0.6));
+	}
+	static Cultivo crearFresa(int tamañoDeseado) {
+		return (new Cultivo ("fresa", tamañoDeseado, 0.3, 0.4, 0.5, 0.4));
+	}
+	
+	//Funciones relacionadas a la produccion
+	
 }
