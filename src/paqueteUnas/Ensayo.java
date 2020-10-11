a5f19f5062f8ac3a20bd31f86ad78deb4df47e84
Sergio <69688291+SergioBzG@users.noreply.github.com>
2020-10-11 01:56:40 -0500

    commit para José

---
+++ src/paqueteUnas/Ensayo.java
@@ -0,0 +1,10 @@
+package paqueteUnas;
+
+public class Ensayo {
+	String jose;
+	public static void main(String[] args) {
+		Ensayo e = new Ensayo();
+		e.jose = "La Wyndoor";
+		System.out.println(e.jose);
+	}
+}
