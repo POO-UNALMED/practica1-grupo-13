574d504f4e8766cf3002b9e17456a60fb99cc681
José <jruedat@unal.edu.co>
2020-10-12 16:38:18 -0500

    Funci0on maikel

--- src/gestorAplicacion/terreno/Cultivo.java
+++ src/gestorAplicacion/terreno/Cultivo.java
@@ -3,6 +3,7 @@ package gestorAplicacion.terreno;
 import gestorAplicacion.Amenaza;
 
 public class Cultivo {
+	
 	private String tipoCultivo;
 	private int tamano;
 	private Amenaza amenaza;
@@ -14,7 +15,6 @@ public class Cultivo {
 	private double fosforo;
 	private double irrigacion;
 	private int cantCampesino;
-	private boolean estadoAmenaza;
 	
 	public Cultivo(String tipoCultivo, int tamano, double nitrogeno, double potasio, double fosforo, double irrigacion, Terreno terreno) {
 		this.tipoCultivo = tipoCultivo; 
@@ -26,31 +26,43 @@ public class Cultivo {
 		this.terreno = terreno;
 	}
 	
+
+	
 	// Se le pide al usuario ingresar el tipo de cultivo que quiere sembrar y el tamaño(dentro del Main), si es papa se ejecuta esta funcion o una de las siguientes:
 	// Se agrega el cultivo a el arreglo "cultivos" en terreno
-	static Cultivo crearPapa(int tamañoDeseado, Terreno terreno) {    
+	// Comprobar capacidad en el Main
+	
+	// public grande
+	
+	
+	public static Cultivo crearPapa(int tamañoDeseado, Terreno terreno) {    
 		Cultivo papa = new Cultivo ("papa", tamañoDeseado, 0.3, 0.4, 0.4, 0.2, terreno);
 		terreno.agregarCultivo(papa);
+		terreno.agregarTipo("papa");
 		return (papa);
 	}
-	static Cultivo crearSandia(int tamañoDeseado, Terreno terreno) {
+	public static Cultivo crearSandia(int tamañoDeseado, Terreno terreno) {
 		Cultivo sandia = new Cultivo ("sandia", tamañoDeseado, 0.1, 0.3, 0.5, 0.5, terreno);
 		terreno.agregarCultivo(sandia);
+		terreno.agregarTipo("sandia");
 		return (sandia);
 	}
-	static Cultivo crearMango(int tamañoDeseado, Terreno terreno) {
+	public static Cultivo crearMango(int tamañoDeseado, Terreno terreno) {
 		Cultivo mango = new Cultivo ("mango", tamañoDeseado, 0.5, 0.3, 0.2, 0.3, terreno);
 		terreno.agregarCultivo(mango);
+		terreno.agregarTipo("mango");
 		return (mango);
 	}
-	static Cultivo crearBanano(int tamañoDeseado, Terreno terreno) {
+	public static Cultivo crearBanano(int tamañoDeseado, Terreno terreno) {
 		Cultivo banano = new Cultivo ("banano", tamañoDeseado, 0.3, 0.1, 0.2, 0.6, terreno);
 		terreno.agregarCultivo(banano);
+		terreno.agregarTipo("banano");
 		return (banano);
 	}
-	static Cultivo crearFresa(int tamañoDeseado, Terreno terreno) {
+	public static Cultivo crearFresa(int tamañoDeseado, Terreno terreno) {
 		Cultivo fresa = new Cultivo ("fresa", tamañoDeseado, 0.3, 0.4, 0.5, 0.4, terreno);
 		terreno.agregarCultivo(fresa);
+		terreno.agregarTipo("fresa");
 		return (fresa);
 	}
 	
@@ -61,4 +73,11 @@ public class Cultivo {
 	public void cultivar() {
 		this.cantProducida += this.cantSembrada;
 	}
+	public Amenaza getAmenaza() {
+		return (this.amenaza);
+	}
+	public void setAmenaza(Amenaza amenazaNueva) {
+		this.amenaza = amenazaNueva;
+	}
+	
 }
--- src/gestorAplicacion/terreno/Terreno.java
+++ src/gestorAplicacion/terreno/Terreno.java
@@ -5,19 +5,36 @@ import java.util.*;
 public class Terreno {
 	private int id;
 	private LinkedList<Cultivo> cultivos = new LinkedList<Cultivo>();
+	private LinkedList<String> tipos = new LinkedList<String>();
 	private int tamano;
 	private double nitrogenoDisponible;
 	private double potasioDisponible;
 	private double fosforoDisponible;
 	private double irrigacionActual;
 	
-	public Terreno(int id, LinkedList<Cultivo> cultivos, int tamano, double nitrogenoDisponible, double potasioDisponible, double fosforoDisponible, double irrigacionActual) {
-		this.id = id; this.cultivos = cultivos; this.tamano = tamano; this.nitrogenoDisponible = nitrogenoDisponible;
-		this.potasioDisponible = potasioDisponible; this.fosforoDisponible = fosforoDisponible; this.irrigacionActual = irrigacionActual;
+	public Terreno(int id, int tamano, double nitrogenoDisponible, double potasioDisponible, double fosforoDisponible, double irrigacionActual) {
+		this.id = id; 
+		this.tamano = tamano;
+		this.nitrogenoDisponible = nitrogenoDisponible;
+		this.potasioDisponible = potasioDisponible; 
+		this.fosforoDisponible = fosforoDisponible; 
+		this.irrigacionActual = irrigacionActual;
+	}
+	//solicitar al usuario el id del terreno que quiere crear
+	public static Terreno crearTerreno(int id) {
+		return (new )
 	}
 	
 	
+	
+	
+	
+	// set cultivo
 	public void agregarCultivo(Cultivo cultivo) {
 		this.cultivos.add(cultivo);
 	}
+	public void agregarTipo(String tipo) {
+		this.tipos.add(tipo);
+	}
+	
 }
