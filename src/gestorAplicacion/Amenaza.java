6458f8eeeb34831f8fe4221bbd557e9b5ac09b9e
Michael200217 <69688194+Michael200217@users.noreply.github.com>
2020-10-12 17:41:49 -0500

    Cambios en amenaza

--- src/gestorAplicacion/Amenaza.java
+++ src/gestorAplicacion/Amenaza.java
@@ -5,16 +5,16 @@ import gestorAplicacion.terreno.*;
 
 public class Amenaza {
 	private String tipoAmenaza;
-	private LinkedList<Object> cultivo = new LinkedList<Object>(); //Se guardará un Cultivo y un Terreno
-	private Pesticida pesticida;
-	private double cantidad;
+	private LinkedList<Cultivo> cultivos = new LinkedList<Cultivo>(); //Se guardaran los Cultivos
+	//Lo hicimos de otra forma, por eso no colocamos pesticida
+	private double cantidad;//Cantidad del cultivo afectada en porcentaje
 	
-	public Amenaza(String tipoAmenaza, LinkedList<Object> cultivo, Pesticida pesticida, double cantidad) {
-		this.tipoAmenaza = tipoAmenaza; this.cultivo = cultivo; this.pesticida = pesticida; this.cantidad = cantidad;
+	public Amenaza(String tipoAmenaza, Cultivo cultivo, double cantidad) {
+		this.tipoAmenaza = tipoAmenaza; this.cultivos.add(cultivo); this.cantidad = cantidad;
+	}	
+	public void atacarCultivo(Cultivo cultivo) {
+		cultivo.setAmenaza(this);
 	}
-	
-	public void atacarCultivo(Terreno terreno, Cultivo cultivo) {}
-	
 	public String getTipo() {
 		return this.tipoAmenaza;
 	}
