359c7419888499a899c02c09417b7e589047b52b
LeimarOcampo <bocampo@unal.edu.co>
2020-10-12 16:33:23 -0500

    Holis

--- src/gestorAplicacion/empleado/Campesino.java
+++ src/gestorAplicacion/empleado/Campesino.java
@@ -1,6 +1,7 @@
 package gestorAplicacion.empleado;
 
 import gestorAplicacion.terreno.*;
+import java.util.*;
 
 public class Campesino extends Empleado{
 	private Cultivo cultivo; //en el que trabja
@@ -8,9 +9,29 @@ public class Campesino extends Empleado{
 	
 	public Campesino(String nombre, int sueldo, int cedula, Cultivo cultivo, Administrador administrador) {
 		super(nombre, sueldo, cedula);
-		this.cultivo = cultivo; this.administrador = administrador; 
+		this.cultivo = cultivo; 
+		this.administrador = administrador; 
 	}
-	
-	public void renunciar() {} //abstaracta 
-	//funciones
+	public void setCultivo(Cultivo cultivo) {
+		this.cultivo = cultivo;
+	}
+	public Cultivo getCultivo() {
+		return cultivo;
+	}
+	public void setAdministrador(Administrador administrador) {
+		this.administrador = administrador;
+	}
+	public Administrador getAdministrador() {
+		return administrador;
+	}
+	public void renunciar(Campesino campesino) {
+		campesino = null;
+	}
+	public void renunciaAleatoria(Campesino campesino) {
+		double x = Math.random();
+		if (x > 0.2) {
+			campesino = null;
+			System.out.println("El campesino: " + this.getNombre() + " ha renunciado.");
+		}
+	}
 }
