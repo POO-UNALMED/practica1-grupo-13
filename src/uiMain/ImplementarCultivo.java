d1daad3a8912776884b1918f092fa3973fec4103
José <jruedat@unal.edu.co>
2020-10-17 18:49:43 -0500

    epa

--- src/uiMain/ImplementarCultivo.java
+++ src/uiMain/ImplementarCultivo.java
@@ -11,8 +11,8 @@ public class ImplementarCultivo {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		
-		Terreno a = new Terreno("12" , 1 , 1 ,1 ,1 ,1);
-		Terreno b = new Terreno("13" , 1 , 1 ,1 ,1 ,1);
+		Terreno a = new Terreno("12" , 100 ,0.1, 0.3, 0.5, 0.5);
+		Terreno b = new Terreno("13" , 200 , 1 ,1 ,1 ,1);
 		Terreno x = new Terreno("14" , 12);
 		
 		
@@ -20,14 +20,18 @@ public class ImplementarCultivo {
 		System.out.println(Terreno.mostrarTerrenos());
 		int entrada = sc.nextInt();
 		Terreno terreno = Terreno.getTerreno().get(entrada - 1);
+		terreno.getCultivoPermitido();     //Funcion que actualiza los tipos disponibles
+		System.out.println(terreno.cultivosPermitidos());     //Funcion que muestra los tipos disponibles
+		
+		System.out.println("Escriba el tipo que quiere sembrar:");
+		String tipo = sc.next();
+		System.out.println("Escriba el tamaño que desea que tenga el cultivo");
+		int tamaño = sc.nextInt();
+		Cultivo.crearCultivo(tipo, tamaño, terreno);
 		
 		
-		System.out.println();
 		
 		
-		System.out.println("Escriba el tipo ");
-		Cultivo.crearCultivo(tipo, tamañoDeseado, terreno);
-
 	
 		
 	
