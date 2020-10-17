a5295a81d8801a60cd223ebed6e7c4fd86a982be
Leimar Ocampo <bocampo@unal.edu.co>
2020-10-17 16:56:59 -0500

    h

--- src/uiMain/Usuario.java
+++ src/uiMain/Usuario.java
@@ -10,28 +10,30 @@ public class Usuario {
 	static String readLine() {
 		return Sc.next();
 	}
-	public static void contratar(int opcionElegida) {
-		String nombre; int sueldo; int cedula; int terreno;
-		System.out.println("Ingrese el nombre:");
-		nombre = readLine();
-		System.out.println("Ingrese el sueldo:");
-		sueldo = readInt();
-		System.out.println("Ingrese el numero de cedula");
-		cedula = readInt();
-		System.out.println("Seleccione un terreno para vincular al trabajador");
-		Terreno.mostrarTerrenos();
-		terreno = readInt();
-		if(opcionElegida == 1) {
-			System.out.println("bobo hijueputa");
-		}
-		else if (opcionElegida == 2) {
-			System.out.println("no sos bobo");
+	public static void contratar() {
+		int opcionElegida;
+		System.out.println("¿Qué trabajador desea contratar?");
+		System.out.println("1. Campesino");
+		System.out.println("2. Agronomo");
+		System.out.println("3. Volver");
+		opcionElegida = Sc.nextInt();
+		if((opcionElegida == 1) || (opcionElegida == 2) && ()) {
+			String nombre; int sueldo; int cedula; int terreno;
+			System.out.println("Ingrese el nombre:");
+			nombre = readLine();
+			System.out.println("Ingrese el sueldo:");
+			sueldo = readInt();
+			System.out.println("Ingrese el numero de cedula");
+			cedula = readInt();
+			System.out.println("Seleccione un terreno para vincular al trabajador");
+			System.out.println(Terreno.mostrarTerrenos());
+			terreno = readInt();
 		}
 		else {
 		}
 	}
 	public static void main(String args[]) {
-		
+		Terreno ter = new Terreno("Cola", 2);
 		int opcionElegida = 0;
 		do {
 			System.out.println("Selecciona una funcion");
@@ -45,14 +47,9 @@ public class Usuario {
 			System.out.println("8. Terminar");
 			opcionElegida = Sc.nextInt();
 			switch (opcionElegida) {
-				case 1: System.out.println("¿Qué trabajador desea contratar?");
-						System.out.println("1. Campesino");
-						System.out.println("2. Agronomo");
-						System.out.println("3. Volver");
-						opcionElegida = Sc.nextInt();
-						contratar(opcionElegida);break;
+				case 1: contratar();break;
 					
 			}
-		}while(opcionElegida != 5);
+		}while(opcionElegida != 8);
 	}
 }
