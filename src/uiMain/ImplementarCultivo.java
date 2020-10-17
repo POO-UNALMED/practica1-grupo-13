e4417b333afdbdc41b74dd833d0c35006886e054
José <jruedat@unal.edu.co>
2020-10-17 18:10:44 -0500

    coso

--- src/uiMain/ImplementarCultivo.java
+++ src/uiMain/ImplementarCultivo.java
@@ -1,40 +1,36 @@
 package uiMain;
 import java.util.Scanner;
 
+import gestorAplicacion.terreno.Cultivo;
 import gestorAplicacion.terreno.Terreno;
 
 public class ImplementarCultivo {
 
-	Scanner sc = new Scanner(System.in);
+	
 	
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		
 		Terreno a = new Terreno("12" , 1 , 1 ,1 ,1 ,1);
 		Terreno b = new Terreno("13" , 1 , 1 ,1 ,1 ,1);
 		Terreno x = new Terreno("14" , 12);
 		
 		
-		System.out.println(" Por favor escoja el terreno en el que quiere sembrar ");
+		System.out.println("Escoja el terreno en el que quiere sembrar");
 		System.out.println(Terreno.mostrarTerrenos());
+		int entrada = sc.nextInt();
+		Terreno terreno = Terreno.getTerreno().get(entrada - 1);
 		
 		
+		System.out.println();
 		
 		
+		System.out.println("Escriba el tipo ");
+		Cultivo.crearCultivo(tipo, tamañoDeseado, terreno);
+
+	
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	
 		
 		
 		
