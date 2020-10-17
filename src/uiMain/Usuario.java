cc06757d8e3d9a223c861bd0acd85cb7dea3a3a9
Leimar Ocampo <bocampo@unal.edu.co>
2020-10-17 03:24:37 -0500

    jj

--- src/uiMain/Usuario.java
+++ src/uiMain/Usuario.java
@@ -1,5 +1,20 @@
 package uiMain;
 
-public class Usuario {
+import java.util.Scanner;
 
+public class Usuario {
+	public static void main(String args[]) {
+		int opcionElegida = 0;
+		while(opcionElegida != 5) {
+			Scanner Sc = new Scanner(System.in);
+			
+			System.out.println("¿Qué funcionalidad desea usar?");
+			System.out.println("1. Contratar");
+			System.out.println("2. Despedir");
+			System.out.println("3. Crear terreno");
+			System.out.println("4. Crear cultivo");
+			opcionElegida = Sc.nextInt();
+			
+		}	
+	}
 }
