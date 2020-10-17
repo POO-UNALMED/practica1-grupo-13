fac08abbcdd2ebd9cc1ff0ca7bee9b9e60925046
Leimar Ocampo <bocampo@unal.edu.co>
2020-10-17 03:10:23 -0500

    Hola

--- src/gestorAplicacion/empleado/Administrador.java
+++ src/gestorAplicacion/empleado/Administrador.java
@@ -2,42 +2,20 @@ package gestorAplicacion.empleado;
 import java.util.*;
 import gestorAplicacion.terreno.*;
 
-public class Administrador extends Empleado{
-	private static boolean activo = false;
-	private static LinkedList<Cultivo> cultivos = new LinkedList<Cultivo>();
-	private static LinkedList<Campesino> campesinos = new LinkedList<Campesino>();
-	public Administrador(String nombre, int sueldo, int cedula) {
-		super(nombre, sueldo, cedula);
-	}
-	public void renunciar() {System.out.println("Hola");} //abstaracta 
-	public void asignacion() {}
-	public void despedir() {
-		Scanner Sc = new Scanner(System.in);
-		for(int i = 0; i <Administrador.getCampesinos().size(); i++) {
-			System.out.println((i+1) + ". " + Administrador.getCampesinos().get(i).getNombre() + " con cédula: " + Administrador.getCampesinos().get(i).getCedula());
-		}
-		int j = Sc.nextInt();
-		Campesino.renunciar(j-1);
-	}
-	public void examinarCultivos() {} //Examina los cultivos presentes en todos los terrenos 
-	public void contratar(String nombre, int sueldo, int cedula) {
-		Campesino campesino = new Campesino(nombre, sueldo, cedula, this); //el admin invoca el contratar
-		agregarCampesinos(campesino);
-	}
-	public static void agregarCultivos(Cultivo cultivo) {
-		cultivos.add(cultivo);
-	}
-	public static LinkedList<Cultivo> getCultivos(){
-		return cultivos;
-	}
-	public static void agregarCampesinos(Campesino campesino) {
-		campesinos.add(campesino);
-	}
-	public static LinkedList<Campesino> getCampesinos(){
-		return campesinos;
-	}
+public class Administrador{
 	public static void main(String args[]) {
-		Scanner Sc = new Scanner(System.in);
-		Administrador admin = new Administrador("Jairo", 10, 10);
+		int opcionElegida = 0;
+		while(opcionElegida != 5) {
+			Scanner Sc = new Scanner(System.in);
+			
+			System.out.println("¿Qué funcionalidad desea usar?");
+			System.out.println("1. Contratar");
+			System.out.println("2. Despedir");
+			System.out.println("3. Crear terreno");
+			System.out.println("4. Crear cultivo");
+			opcionElegida = Sc.nextInt();
+			
+		}
+		
 	}
 }
--- src/gestorAplicacion/empleado/Agronomo.java
+++ src/gestorAplicacion/empleado/Agronomo.java
@@ -1,5 +1,6 @@
 package gestorAplicacion.empleado;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 import gestorAplicacion.terreno.*;
 
@@ -11,5 +12,30 @@ public class Agronomo extends Empleado{
 	public Agronomo(String nombre, int sueldo, int cedula, Terreno terreno) {
 		super(nombre, sueldo, cedula);
 		this.terreno = terreno;
+	}
+	public void asignacion() {}
+	public void despedir() {
+		for(int i = 0; i <Administrador.getCampesinos().size(); i++) {
+			System.out.println((i+1) + ". " + Administrador.getCampesinos().get(i).getNombre() + " con cédula: " + Administrador.getCampesinos().get(i).getCedula());
+		}
+		int j = Sc.nextInt();
+		Campesino.renunciar(j-1);
+	}
+	public void examinarCultivos() {} //Examina los cultivos presentes en todos los terrenos 
+	public void contratar(String nombre, int sueldo, int cedula) {
+		Campesino campesino = new Campesino(nombre, sueldo, cedula, this); //el admin invoca el contratar
+		agregarCampesinos(campesino);
+	}
+	public static void agregarCultivos(Cultivo cultivo) {
+		cultivos.add(cultivo);
+	}
+	public static LinkedList<Cultivo> getCultivos(){
+		return cultivos;
+	}
+	public static void agregarCampesinos(Campesino campesino) {
+		campesinos.add(campesino);
+	}
+	public static LinkedList<Campesino> getCampesinos(){
+		return campesinos;
 	}
 }
