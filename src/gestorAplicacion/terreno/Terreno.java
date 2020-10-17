5418c55d3ab2d0325fa0ecb73d320b090915ee91
José <jruedat@unal.edu.co>
2020-10-17 14:43:24 -0500

    1 static que me faltó jejejejejeje

--- src/gestorAplicacion/terreno/Terreno.java
+++ src/gestorAplicacion/terreno/Terreno.java
@@ -95,7 +95,7 @@ public class Terreno {
 	public String getId() {
 		return (this.id);
 	}
-	public String mostrarTerrenos() {       //saca el linkedList terrenosTotales, y le muestra al usuario sus ids
+	public static String mostrarTerrenos() {       //saca el linkedList terrenosTotales, y le muestra al usuario sus ids
 		String muestra = "";
 		for (int i = 0; i < terrenosTotales.size(); i ++){
 			muestra = muestra + terrenosTotales.get(i).getId() + " ";
