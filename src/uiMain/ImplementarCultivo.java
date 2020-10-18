0978162b031060af818e3a150f42765e43d762e7
José <jruedat@unal.edu.co>
2020-10-17 21:52:27 -0500

    aa

--- src/uiMain/ImplementarCultivo.java
+++ src/uiMain/ImplementarCultivo.java
@@ -5,44 +5,34 @@ import gestorAplicacion.terreno.Cultivo;
 import gestorAplicacion.terreno.Terreno;
 
 public class ImplementarCultivo {
-
-	
 	
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		
-		Terreno a = new Terreno("12" , 1000 ,0.3, 0.5, 0.6, 0.1);
-
+		System.out.println("Escoja el id del terreno en el que quiere sembrar");
 		
-		System.out.println("Escoja el terreno en el que quiere sembrar");
-		System.out.println(Terreno.mostrarTerrenos());
-		int entrada = sc.nextInt();
-		
-		Terreno terreno = Terreno.getTerreno().get(entrada - 1);
-		terreno.getCultivoPermitido();     //Funcion que actualiza los tipos disponibles
-		System.out.println(terreno.cultivosPermitidos());     //Funcion que muestra los tipos disponibles
-		
-		System.out.println("Escriba el tipo que quiere sembrar:");
-		String tipo = sc.next();
-		System.out.println("Escriba el tamaño que desea que tenga el cultivo");
-		int tamaño = sc.nextInt();
-		
-		Cultivo.crearCultivo(tipo, tamaño, terreno);
-		
-		
-		
-		
-	    
-		
-	
+		if (Terreno.getTerreno().size() !=0 ) {
+			System.out.println(Terreno.mostrarTerrenos());
+			int entrada = sc.nextInt();
+			Terreno terreno = Terreno.getTerreno().get(entrada - 1);
+			terreno.getCultivoPermitido();     //Funcion que actualiza los tipos disponibles
+			
+			if (terreno.getCultivoPermitido().size() != 0) {
+				System.out.println(terreno.cultivosPermitidos());
+				System.out.println("Escriba el tipo que quiere sembrar:");
+				String tipo = sc.next();
+				System.out.println("Escriba el tamaño que desea que tenga el cultivo");
+				int tamaño = sc.nextInt();
+				Cultivo.crearCultivo(tipo, tamaño, terreno);
+			}else {
+				System.out.println("No dispone con los requerimientos suficientes para sembrar en este terreno, por favor irrigue o fertilice");
+			}
+			
+		}else {
+			System.out.println("No dispone de terrenos, por favor cree uno");
+		}
 		
 		
 		
 	}
-	
-	
-	
-	
-	
-	
 }
