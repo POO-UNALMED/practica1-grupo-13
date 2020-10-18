c2a15236bf5ca9016a378a147edc9138ed4091d2
Leimar Ocampo <bocampo@unal.edu.co>
2020-10-18 00:11:02 -0500

    Update Usuario.java

--- src/uiMain/Usuario.java
+++ src/uiMain/Usuario.java
@@ -90,13 +90,37 @@ public class Usuario {
 		}
 		
 	}
-	public static int produccion() {
-		return 1;
+	public static void produccion() {
+		System.out.println("La cantidad de cultivos de cada clase es:");
+		System.out.println("Cantidad de papas: " + + "hectareas.");
+		System.out.println("Cantidad de sandias: " + + "hectareas.");
+		System.out.println("Cantidad de mangos: " + + "hectareas.");
+		System.out.println("Cantidad de bananos: " + + "hectareas.");
+		System.out.println("Cantidad de fresas: " + + "hectareas.");
 	}
 	public static void examinarCultivo(){
-		System.out.println("Seleccione un cultivo:");
-		System.out.println("");
-		System.out.println();
+		if(Cultivo.getCultivos().isEmpty()) {
+			System.out.println("Debe cultivar primero");
+		}
+		else {
+			int opcionElegida;
+			System.out.println("Seleccione un cultivo:");
+			System.out.println(Cultivo.mostrarCultivos());
+			opcionElegida = readInt() - 1;
+			System.out.println(Cultivo.getCultivos().get(opcionElegida));
+			if(!Cultivo.getCultivos().get(opcionElegida).getAmenaza().equals(null)) {
+				System.out.print("El cultivo se encuentra bajo una amenaza de: ");
+				System.out.println(Cultivo.getCultivos().get(opcionElegida).getAmenaza());
+				System.out.println("¿Desea aplicas pesticida para eliminar la amenaza del cultivo?");
+				System.out.println("1. Si");
+				System.out.println("2. No");
+				opcionElegida = readInt();
+				if (opcionElegida == 1) {
+					
+				}
+				else {}
+			}
+		}
 	}
 	public static void main(String args[]) {
 		new Terreno("Cola", 2);
@@ -106,7 +130,7 @@ public class Usuario {
 			System.out.println("Selecciona una funcion");
 			System.out.println("1. Contratar");
 			System.out.println("2. Despedir");
-			System.out.println("3. Produccion por terreno");
+			System.out.println("3. Produccion total de los cultivos");
 			System.out.println("4. Examinar cultivos");
 			System.out.println("5. Cultivar");
 			System.out.println("6. Agregar terreno");
