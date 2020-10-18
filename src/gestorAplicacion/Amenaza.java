3a70d5b3a0a009b138cac9e227dd44a1228940ec
Michael200217 <69688194+Michael200217@users.noreply.github.com>
2020-10-18 00:39:37 -0500

    d

--- src/gestorAplicacion/Amenaza.java
+++ src/gestorAplicacion/Amenaza.java
@@ -6,8 +6,7 @@ import gestorAplicacion.terreno.*;
 public class Amenaza {
 	private String tipoAmenaza;
 	private Cultivo cultivo; //Se guardaran los Cultivos
-	//Lo hicimos de otra forma, por eso no colocamos pesticida
-	private double cantidad;//Cantidad del cultivo afectada en porcentaje
+	private double cantidad;//Cantidad del cultivo afectada
 	
 	public Amenaza(String tipoAmenaza, Cultivo cultivo, double cantidad) {
 		this.tipoAmenaza = tipoAmenaza; this.cultivo=cultivo; this.cantidad=cantidad;
