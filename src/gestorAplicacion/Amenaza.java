f9f7e61e20e45b006cbc3eb02b0401966afbb69c
Michael200217 <69688194+Michael200217@users.noreply.github.com>
2020-10-17 23:02:22 -0500

    Amenaza trin

--- src/gestorAplicacion/Amenaza.java
+++ src/gestorAplicacion/Amenaza.java
@@ -9,13 +9,44 @@ public class Amenaza {
 	//Lo hicimos de otra forma, por eso no colocamos pesticida
 	private double cantidad;//Cantidad del cultivo afectada en porcentaje
 	
-	public Amenaza(String tipoAmenaza, Cultivo cultivo, double cantidad) {
-		this.tipoAmenaza = tipoAmenaza; this.cultivos.add(cultivo); this.cantidad = cantidad;
+	public Amenaza(String tipoAmenaza, Cultivo cultivo) {
+		this.tipoAmenaza = tipoAmenaza; this.cultivos.add(cultivo);
 	}	
-	public void atacarCultivo(Cultivo cultivo) {
-		cultivo.setAmenaza(this);
+	public static void atacarCultivo(Amenaza amenaza,Cultivo cultivo,double Cantidad) {
+		cultivo.setAmenaza(amenaza);
+		int Random_Para_Cantidad_Infectada=(int) ((int)Math.random()*Cantidad);
+		cultivo.setCantSembrada(Random_Para_Cantidad_Infectada);
 	}
 	public String getTipo() {
 		return this.tipoAmenaza;
+	}
+	public static void Amenazar() {
+		double Random_Para_AMENAZAR=Math.random();
+		if(Random_Para_AMENAZAR>=0 && Random_Para_AMENAZAR<=0.4){
+			double Random_Para_TipoPeste=Math.random();
+			LinkedList<Cultivo>cultivatrones=Cultivo.getCultivos();
+			String TipoPeste;
+			int cantidad_de_cultivos=cultivatrones.size();
+			if(cantidad_de_cultivos!=0) {
+				int Random_Para_Sacar_Cultivo=(int)Random_Para_TipoPeste*cantidad_de_cultivos;
+				if(cultivatrones.get(Random_Para_Sacar_Cultivo).getAmenaza()==null && cultivatrones.get(Random_Para_Sacar_Cultivo).getCantSembrada()>0) {
+					if(Random_Para_TipoPeste>=0 && Random_Para_TipoPeste<=0.3) {
+						TipoPeste="Plaga";
+						Amenaza amenaza=new Amenaza(TipoPeste,cultivatrones.get(Random_Para_Sacar_Cultivo));
+						atacarCultivo(amenaza,cultivatrones.get(Random_Para_Sacar_Cultivo),cultivatrones.get(Random_Para_Sacar_Cultivo).getCantSembrada());
+					}
+					else if(Random_Para_TipoPeste>0.3 && Random_Para_TipoPeste<0.6) {
+						TipoPeste="Maleza";
+						Amenaza amenaza=new Amenaza(TipoPeste,cultivatrones.get(Random_Para_Sacar_Cultivo));
+						atacarCultivo(amenaza,cultivatrones.get(Random_Para_Sacar_Cultivo),cultivatrones.get(Random_Para_Sacar_Cultivo).getCantSembrada());
+					}
+					else {
+						TipoPeste="Hongo";
+						Amenaza amenaza=new Amenaza(TipoPeste,cultivatrones.get(Random_Para_Sacar_Cultivo));
+						atacarCultivo(amenaza,cultivatrones.get(Random_Para_Sacar_Cultivo),cultivatrones.get(Random_Para_Sacar_Cultivo).getCantSembrada());
+					}
+				}
+			}
+		}
 	}
 }
