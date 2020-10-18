f96123b71ad890a204c67861696f2ded75ba713a
sacastrot <70394887+sacastrot@users.noreply.github.com>
2020-10-18 02:44:48 -0500

    Update Empleado.java
    Sobrecarga de constructuroes

--- src/gestorAplicacion/empleado/Empleado.java
+++ src/gestorAplicacion/empleado/Empleado.java
@@ -1,36 +1,57 @@
 package gestorAplicacion.empleado;
+
 import gestorAplicacion.terreno.*;
-public abstract class Empleado  {
+
+public abstract class Empleado {
 	protected String nombre;
 	protected int sueldo;
 	protected int cedula;
 	protected Terreno terreno;
-	
+
 	public Empleado(String nombre, int sueldo, int cedula, Terreno terreno) {
-		this.nombre = nombre; this.sueldo = sueldo; this.cedula = cedula; this.terreno = terreno;
+		this.nombre = nombre;
+		this.sueldo = sueldo;
+		this.cedula = cedula;
+		this.terreno = terreno;
 	}
+
+	public Empleado(String nombre, int cedula, Terreno terreno) {
+		this.nombre = nombre;
+		this.sueldo = 1000000;
+		this.cedula = cedula;
+		this.terreno = terreno;
+	}
+
 	public abstract void renunciar();
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setSueldo(int sueldo) {
 		this.sueldo = sueldo;
 	}
+
 	public int getSueldo() {
 		return sueldo;
 	}
+
 	public int getCedula() {
 		return cedula;
 	}
+
 	public void setCedula(int cedula) {
 		this.cedula = cedula;
 	}
+
 	public void setTerreno(Terreno terreno) {
 		this.terreno = terreno;
 	}
+
 	public Terreno getTerreno() {
 		return this.terreno;
 	}
