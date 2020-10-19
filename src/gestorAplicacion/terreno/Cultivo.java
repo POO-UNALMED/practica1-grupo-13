5e4d21fc6a1318c48e5d7449da9bd31af8d82af6
Sergio <69688291+SergioBzG@users.noreply.github.com>
2020-10-18 19:44:50 -0500

    Disculpame José

--- src/gestorAplicacion/terreno/Cultivo.java
+++ src/gestorAplicacion/terreno/Cultivo.java
@@ -154,5 +154,13 @@ public class Cultivo {
 		return (muestra);
 	}
 
-	
+	public String toString() {
+		if(this.getAmenaza() == null) {
+			return "El cultivo de tipo " + this.getTipoCultivo() + " ubicado en el terreno " + this.getTerreno().getId() + " se encuentra bajo amenaza.\n " +
+		    "La amenaza que lo ataca es es de tipo " + this.getAmenaza().getTipo();
+		}
+		else {
+			return "El cultivo de tipo " + this.getTipoCultivo() + " ubicado en el terreno " + this.getTerreno().getId() + " no se encuentra bajo amenaza";
+		}
+	}
 }
