327f64083937e263bbc86b108664d2fae10b2dbf
sacastrot <70394887+sacastrot@users.noreply.github.com>
2020-10-28 13:54:41 -0500

    Update SerializacionC.java, SerializacionG.java
    Documentacion

--- src/BaseDatos/SerializacionC.java
+++ src/BaseDatos/SerializacionC.java
@@ -7,14 +7,22 @@ import java.io.ObjectInputStream;
 import java.util.LinkedList;
 import gestorAplicacion.empleado.*;
 import gestorAplicacion.terreno.*;
-
+/**
+ * Clase encargada de escribir las instancias antes serializadas
+ *
+ */
 public class SerializacionC {
+	/** Abre el flujo de datos*/
 	private static ObjectInputStream entradas;
+	/** Crea un nuevo fichero*/
 	static File fichero = new File("");
+	/** Objeto usado para escribir todas las instancias*/
 	static Object Objeto;
 
 	public static void Load() {
-		
+		/** 
+		 * Se cargan las instancias de de cultivos
+		 */
 		try {
 			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath() + "\\src\\BaseDatos\\temp\\Cultivos.txt"));
 			Objeto = entradas.readObject();
@@ -29,7 +37,10 @@ public class SerializacionC {
 		} catch (ClassNotFoundException e) {
 			
 		} 
-		
+		/**
+		 * Se escriben las instancias de Terrenos previamente creadas y se agrega a la lista de agronomos 
+		 * los que estan asociados a los terrenos
+		 */
 		try {
 			entradas = new ObjectInputStream(new FileInputStream(fichero.getAbsolutePath() + "\\src\\BaseDatos\\temp\\Terrenos.txt"));
 			Objeto = entradas.readObject();
